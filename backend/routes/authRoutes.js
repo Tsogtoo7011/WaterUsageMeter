86394b985de53849cb1e90ed3632a488af34ef4c
B210910026 <tsogtoo7011@gmail.com>
2025-03-28 11:20:22 +0800

    Fix at backend

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 const { 
-  signup, 
   verifyEmail, 
   resendVerificationEmail 
 } = require('../controllers/authController');
